8f5d509d7ff2acc997208e6ad6c19d1f7a92be5d
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-12-19 22:10:01 +0530

    Update shizukabot.py

--- shizuka/ShizukaChatBot/shizukabot.py
+++ shizuka/ShizukaChatBot/shizukabot.py
@@ -1,30 +1,23 @@
 import asyncio
 import re
-
 import aiohttp
 import emoji
 import requests
-from coffeehouse.exception import CoffeeHouseError as CFError
 from gpytranslate import Translator
-from pyrogram import Client
-from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton
-from pyrogram.types import InlineKeyboardMarkup
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from shizuka import SHIZUKA
 
 url = "https://acobot-brainshop-ai-v1.p.rapidapi.com/get"
-
 translator = Translator()
 BOT_ID = 1699240021
 
-
 def extract_emojis(s):
-    return "".join(c for c in s if c in emoji.UNICODE_EMOJI)
-
+    # Fixed for emoji 2.0+
+    return "".join(c for c in s if c in emoji.EMOJI_DATA)
 
 en_chats = []
-
 
 @SHIZUKA.on_message(
     filters.text & filters.reply & ~filters.bot & ~filters.via_bot & ~filters.forwarded,
@@ -33,171 +26,118 @@ en_chats = []
 async def lycia(client, message):
     if message.reply_to_message.from_user.id != BOT_ID:
         message.continue_propagation()
+    
     msg = message.text
     chat_id = message.chat.id
     hello = message.from_user.id
+    
     if msg.startswith("/") or msg.startswith("@"):
         message.continue_propagation()
+        
     if chat_id in en_chats:
         onik = msg
         querystring = {
             "bid": "161901",
             "key": "Sgv5QAk5wEbhqYn0",
-            "uid": hello,
-            "msg": {test},
+            "uid": str(hello),
+            "msg": onik,
         }
         headers = {
             "X-RapidAPI-Host": "acobot-brainshop-ai-v1.p.rapidapi.com",
             "X-RapidAPI-Key": "a1fa7cb243msh40ac83d27b168ddp1fdc80jsn7d0db8bffc62",
         }
-        response = requests.request(
-            "GET",
-            url,
-            headers=headers,
-            params=querystring,
-        )
-        result = response.text
-        result = result.replace('{"cnt":"', "")
-        result = result.replace('"}', "")
-        result = result.replace("<a href=\\", "<a href =")
-        result = result.replace(r"<\/a>", "</a>")
-        saini = result
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        result = response.json().get("cnt", "I'm not sure what to say.")
+        
         try:
             await SHIZUKA.send_chat_action(message.chat.id, "typing")
-            await message.reply_text(saini)
-        except CFError as e:
+            await message.reply_text(result)
+        except Exception as e:
             print(e)
     else:
         u = msg.split()
         emj = extract_emojis(msg)
-        msg = msg.replace(emj, "")
-        if (
-            [(k) for k in u if k.startswith("@")]
-            and [(k) for k in u if k.startswith("#")]
-            and [(k) for k in u if k.startswith("/")]
-            and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
-        ):
-            h = " ".join(filter(lambda x: x[0] != "@", u))
-            km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
-            tm = km.split()
-            jm = " ".join(filter(lambda x: x[0] != "#", tm))
-            hm = jm.split()
-            rm = " ".join(filter(lambda x: x[0] != "/", hm))
-        elif [(k) for k in u if k.startswith("@")]:
-            rm = " ".join(filter(lambda x: x[0] != "@", u))
-        elif [(k) for k in u if k.startswith("#")]:
-            rm = " ".join(filter(lambda x: x[0] != "#", u))
-        elif [(k) for k in u if k.startswith("/")]:
-            rm = " ".join(filter(lambda x: x[0] != "/", u))
-        elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
-            rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
-        else:
-            rm = msg
-            lan = await translator.detect(rm)
+        rm = msg.replace(emj, "")
+        
+        # Simple cleanup for mentions/hashtags
+        rm = " ".join(filter(lambda x: not x.startswith(("@", "#", "/")), u))
+        
+        detection = await translator.detect(rm)
+        lan = detection if isinstance(detection, str) else detection.lang
+        
         onik = rm
-        if not "en" in lan and not lan == "":
-            onik = translator.translate(onik, targetlang="en")
+        if lan != "en":
+            tr_res = await translator.translate(onik, targetlang="en")
+            onik = tr_res.text
 
         querystring = {
             "bid": "161901",
             "key": "Sgv5QAk5wEbhqYn0",
-            "uid": hello,
-            "msg": {onik},
+            "uid": str(hello),
+            "msg": onik,
         }
         headers = {
             "X-RapidAPI-Host": "acobot-brainshop-ai-v1.p.rapidapi.com",
             "X-RapidAPI-Key": "a1fa7cb243msh40ac83d27b168ddp1fdc80jsn7d0db8bffc62",
         }
-        response = requests.request(
-            "GET",
-            url,
-            headers=headers,
-            params=querystring,
-        )
-        result = response.text
-        result = result.replace('{"cnt":"', "")
-        result = result.replace('"}', "")
-        result = result.replace("<a href=\\", "<a href =")
-        result = result.replace(r"<\/a>", "</a>")
-        saini = result
-        if not "en" in lan and not lan == "":
-            pro = translator.translate(saini, lang_tgt=lan[0])
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        saini = response.json().get("cnt", "Error connecting to brain.")
+
+        if lan != "en":
+            tr_back = await translator.translate(saini, targetlang=lan)
+            saini = tr_back.text
+
         try:
             await SHIZUKA.send_chat_action(message.chat.id, "typing")
             await message.reply_text(saini)
-        except CFError as e:
+        except Exception as e:
             print(e)
 
-
-@SHIZUKA.on_message(filters.text & filters.private & ~filters.reply & ~filters.bot)
+@SHIZUKA.on_message(filters.text & filters.private & ~filters.bot)
 async def chankit(client, message):
     msg = message.text
     hello = message.from_user.id
     if msg.startswith("/") or msg.startswith("@"):
         message.continue_propagation()
+    
     u = msg.split()
     emj = extract_emojis(msg)
-    msg = msg.replace(emj, "")
-    if (
-        [(k) for k in u if k.startswith("@")]
-        and [(k) for k in u if k.startswith("#")]
-        and [(k) for k in u if k.startswith("/")]
-        and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
-    ):
-        h = " ".join(filter(lambda x: x[0] != "@", u))
-        km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
-        tm = km.split()
-        jm = " ".join(filter(lambda x: x[0] != "#", tm))
-        hm = jm.split()
-        rm = " ".join(filter(lambda x: x[0] != "/", hm))
-    elif [(k) for k in u if k.startswith("@")]:
-        rm = " ".join(filter(lambda x: x[0] != "@", u))
-    elif [(k) for k in u if k.startswith("#")]:
-        rm = " ".join(filter(lambda x: x[0] != "#", u))
-    elif [(k) for k in u if k.startswith("/")]:
-        rm = " ".join(filter(lambda x: x[0] != "/", u))
-    elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
-        rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
-    else:
-        rm = msg
-        lan = await translator.detect(rm)
+    rm = msg.replace(emj, "")
+    rm = " ".join(filter(lambda x: not x.startswith(("@", "#", "/")), u))
+    
+    detection = await translator.detect(rm)
+    lan = detection if isinstance(detection, str) else detection.lang
+    
     onik = rm
-    if not "en" in lan and not lan == "":
-        onik = translator.translate(onik, targetlang="en")
+    if lan != "en":
+        tr_res = await translator.translate(onik, targetlang="en")
+        onik = tr_res.text
 
     querystring = {
         "bid": "161901",
         "key": "Sgv5QAk5wEbhqYn0",
-        "uid": hello,
-        "msg": {onik},
+        "uid": str(hello),
+        "msg": onik,
     }
     headers = {
         "X-RapidAPI-Host": "acobot-brainshop-ai-v1.p.rapidapi.com",
         "X-RapidAPI-Key": "a1fa7cb243msh40ac83d27b168ddp1fdc80jsn7d0db8bffc62",
     }
-    response = requests.request(
-        "GET",
-        url,
-        headers=headers,
-        params=querystring,
-    )
-    result = response.text
-    result = result.replace('{"cnt":"', "")
-    result = result.replace('"}', "")
-    result = result.replace("<a href=\\", "<a href =")
-    result = result.replace(r"<\/a>", "</a>")
-    saini = result
-    if not "en" in lan and not lan == "":
-        saini = translator.translate(saini, targetlang=lan[0])
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    saini = response.json().get("cnt", "...")
+
+    if lan != "en":
+        tr_back = await translator.translate(saini, targetlang=lan)
+        saini = tr_back.text
+
     try:
         await SHIZUKA.send_chat_action(message.chat.id, "typing")
         await message.reply_text(saini)
-    except CFError as e:
+    except Exception as e:
         print(e)
 
-
 @SHIZUKA.on_message(
-    filters.regex("Lycia|lycia|SHIZUKA|shizuka||Shizuka")
+    filters.regex("(?i)(Lycia|SHIZUKA|shizuka)")
     & ~filters.bot
     & ~filters.via_bot
     & ~filters.forwarded
@@ -206,63 +146,40 @@ async def chankit(client, message):
 )
 async def chankitsaini(client, message):
     msg = message.text
+    hello = message.from_user.id
     if msg.startswith("/") or msg.startswith("@"):
         message.continue_propagation()
+        
     u = msg.split()
-    emj = extract_emojis(msg)
-    msg = msg.replace(emj, "")
-    if (
-        [(k) for k in u if k.startswith("@")]
-        and [(k) for k in u if k.startswith("#")]
-        and [(k) for k in u if k.startswith("/")]
-        and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
-    ):
-        h = " ".join(filter(lambda x: x[0] != "@", u))
-        km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
-        tm = km.split()
-        jm = " ".join(filter(lambda x: x[0] != "#", tm))
-        hm = jm.split()
-        rm = " ".join(filter(lambda x: x[0] != "/", hm))
-    elif [(k) for k in u if k.startswith("@")]:
-        rm = " ".join(filter(lambda x: x[0] != "@", u))
-    elif [(k) for k in u if k.startswith("#")]:
-        rm = " ".join(filter(lambda x: x[0] != "#", u))
-    elif [(k) for k in u if k.startswith("/")]:
-        rm = " ".join(filter(lambda x: x[0] != "/", u))
-    elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
-        rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
-    else:
-        rm = msg
-        lan = await translator.detect(rm)
+    rm = " ".join(filter(lambda x: not x.startswith(("@", "#", "/")), u))
+    
+    detection = await translator.detect(rm)
+    lan = detection if isinstance(detection, str) else detection.lang
+    
     onik = rm
-    if not "en" in lan and not lan == "":
-        onik = await translator.translate(onik, targetlang="en")
+    if lan != "en":
+        tr_res = await translator.translate(onik, targetlang="en")
+        onik = tr_res.text
+
     querystring = {
         "bid": "161901",
         "key": "Sgv5QAk5wEbhqYn0",
-        "uid": hello,
-        "msg": {onik},
+        "uid": str(hello),
+        "msg": onik,
     }
     headers = {
         "X-RapidAPI-Host": "acobot-brainshop-ai-v1.p.rapidapi.com",
         "X-RapidAPI-Key": "a1fa7cb243msh40ac83d27b168ddp1fdc80jsn7d0db8bffc62",
     }
-    response = requests.request(
-        "GET",
-        url,
-        headers=headers,
-        params=querystring,
-    )
-    result = response.text
-    result = result.replace('{"cnt":"', "")
-    result = result.replace('"}', "")
-    result = result.replace("<a href=\\", "<a href =")
-    result = result.replace(r"<\/a>", "</a>")
-    pro = result
-    if not "en" in lan and not lan == "":
-        saini = translator.translate(saini, targetlang=lan[0])
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    saini = response.json().get("cnt", "Yes?")
+
+    if lan != "en":
+        tr_back = await translator.translate(saini, targetlang=lan)
+        saini = tr_back.text
+
     try:
         await SHIZUKA.send_chat_action(message.chat.id, "typing")
         await message.reply_text(saini)
-    except CFError as e:
+    except Exception as e:
         print(e)
